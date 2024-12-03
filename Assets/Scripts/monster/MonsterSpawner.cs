fcd3d8aa10065478ee9e34b5cb36691026ea7b0a
IsmailTheGreatest <2110049@newuzbekistanuniversity.uz>
2024-12-03 10:28:12 +0500

    monster script changed

--- Assets/Scripts/monster/MonsterSpawner.cs
+++ Assets/Scripts/monster/MonsterSpawner.cs
@@ -8,6 +8,8 @@ public class MonsterSpawner : MonoBehaviour
     public float spawnRate = 2.0f; // Time interval between spawns
     public Vector2 spawnAreaMin; // Minimum x and y coordinates for the spawn area
     public Vector2 spawnAreaMax; // Maximum x and y coordinates for the spawn area
+    public int maxMonsters = 30; // Maximum number of monsters to spawn
+    private int currentMonsters = 0; // Current number of spawned monsters
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +17,18 @@ public class MonsterSpawner : MonoBehaviour
         StartCoroutine(SpawnMonsters());
     }
 
+    void setMonsterNumber(int number)
+    {
+        maxMonsters = number;
+    }
+
     // Coroutine to spawn monsters at regular intervals
     IEnumerator SpawnMonsters()
     {
-        while (true)
+        while (currentMonsters < maxMonsters)
         {
             SpawnMonster();
+            currentMonsters++;
             yield return new WaitForSeconds(spawnRate);
         }
     }
@@ -28,9 +36,10 @@ public class MonsterSpawner : MonoBehaviour
     // Method to spawn a monster at a random position within the spawn area
     void SpawnMonster()
     {
-        float randomX = Random.Range(spawnAreaMin.x, spawnAreaMax.x);
-        float randomY = Random.Range(spawnAreaMin.y, spawnAreaMax.y);
-        Vector2 spawnPosition = new Vector2(randomX, randomY);
+        Vector2 spawnPosition = new Vector2(
+            Random.Range(spawnAreaMin.x, spawnAreaMax.x),
+            Random.Range(spawnAreaMin.y, spawnAreaMax.y)
+        );
         Instantiate(monsterPrefab, spawnPosition, Quaternion.identity);
     }
 }
